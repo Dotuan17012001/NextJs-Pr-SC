de786737aa16e214cc679715cb36c746171edb5b
Dotuan17012001 <dovantuan17012001@gmail.com>
2024-02-27 14:19:43 +0700

    display table user antd

--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -1,21 +1,22 @@
 import { useEffect, useState } from "react";
-import "../../styles/users.css";
+// import "../../styles/users.css";
+import { Table } from "antd";
+import type { TableColumnsType } from "antd";
 
 interface IUser {
-  _id:number,
-  email:string,
-  name:string,
-  role:string
+  _id: number;
+  email: string;
+  name: string;
+  role: string;
 }
 
 const UserTable = () => {
-  const [listUser, setListUser] = useState([])
+  const [listUser, setListUser] = useState([]);
   useEffect(() => {
     getData();
   }, []);
 
   const getData = async () => {
-    
     const access_token =
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjVkMDI1MjgyZmZlMzJkMTg3Y2E3NWUwIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIGFkbWluIiwidHlwZSI6IlNZU1RFTSIsInJvbGUiOiJBRE1JTiIsImdlbmRlciI6Ik1BTEUiLCJhZ2UiOjY5LCJpYXQiOjE3MDgzMjA5MTQsImV4cCI6MTc5NDcyMDkxNH0.SUgtJt-lbnD_Lh8wdtAd_RNhm4i3_Hgus1yJuHksq2Y";
 
@@ -28,37 +29,39 @@ const UserTable = () => {
     });
 
     const dataRes = await res.json();
-    if(dataRes && dataRes.data.result){
+    if (dataRes && dataRes.data.result) {
       setListUser(dataRes.data.result);
     }
-    console.log("data", dataRes);
+    console.log("data", dataRes.data.result);
   };
+
+  const columns: TableColumnsType<IUser> = [
+    {
+      title: "Email",
+      dataIndex: "email",
+      render: (value, record) => {
+        return(
+          <div>{record.email}</div>
+        )
+      }
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Role",
+      dataIndex: "role",
+    },
+    
+  ];
   return (
     <div>
       <h2>Table User</h2>
-      <table>
-        <thead>
-          <tr>
-            <td>Email</td>
-            <td>Name</td>
-            <td>Role</td>
-          </tr>
-        </thead>
-        <tbody>
-          {listUser?.map((user : IUser, index) => {
-            return (
-          
-            <tr key={user?._id}>
-              <td>{user?.email}</td>
-              <td>{user?.name}</td>
-              <td>{user?.role}</td>
-            </tr>
-            
-            )
-          })}
-          
-        </tbody>
-      </table>
+      <Table
+        columns={columns}
+        dataSource={listUser}
+      />
     </div>
   );
 };
